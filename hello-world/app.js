2d32942c21005a53f9c20de24f846cd1ae7e8e22
wong.chris <wong.chris@principal.com>
2021-05-06 11:50:24 +0800

    v3

--- hello-world/app.js
+++ hello-world/app.js
@@ -20,7 +20,7 @@ exports.lambdaHandler = async (event, context) => {
     response = {
       statusCode: 200,
       body: JSON.stringify({
-        message: `v2 - ${new Date()}`,
+        message: `v3 - ${new Date()}`,
         // location: ret.data.trim()
       }),
     };
